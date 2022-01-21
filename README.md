1ea283078081a77ad92f317c713c43d5674c961a
Yao Eloge <yaoeloge@outlook.fr>
2022-01-21 02:54:20 +0000

    version vue.js

--- README.md
+++
@@ -1,1 +0,0 @@
-"# ji_parrainage" 
---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <swiper
+    :effect="'cube'"
+    :grabCursor="true"
+    :cubeEffect="{
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 20,
+      shadowScale: 0.94,
+    }"
+    :pagination="true"
+    class="mySwiper">
+    <swiper-slide>
+      <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+      </swiper-slide>
+    <swiper-slide>
+      <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+      </swiper-slide>
+    <swiper-slide>
+      <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+      </swiper-slide>
+    <swiper-slide>
+      <img src="https://swiperjs.com/demos/images/nature-4.jpg"/>
+      </swiper-slide>
+  </swiper>
+</template>
+<script>
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+// Import Swiper styles
+import "swiper/css";
+
+import "swiper/css/effect-cube";
+import "swiper/css/pagination";
+
+import "./style.css";
+
+// import Swiper core and required modules
+import SwiperCore, { EffectCube, Pagination } from "swiper";
+
+// install Swiper modules
+SwiperCore.use([EffectCube, Pagination]);
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
