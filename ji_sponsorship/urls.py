2d89afc640efdb8786a76c0b41488a67a693539c
Yao Eloge <yaoeloge@outlook.fr>
2022-01-25 00:56:52 +0000

    register

--- ji_sponsorship/urls.py
+++ ji_sponsorship/urls.py
@@ -1,11 +1,13 @@
 """ji_sponsorship URL Configuration
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from ji_sponsorship import settings
 
 from sponsorship.views import index
 
 urlpatterns = [
     path('', index, name="index"),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
